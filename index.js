d369faebc2129ccb349af9a1c2c2a55f68d8f2a8
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-30 17:10:33 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningRecord = record.find(recordEntry => recordEntry.result === "W");
+    return winningRecord ? winningRecord.year : undefined;
+  }
+  
+
